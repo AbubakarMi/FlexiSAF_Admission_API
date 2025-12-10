c01d123cc7b35637b83e82a3b46dfd243e98cf5c
Abubakar Mi <abubakarmi131@gmail.com>
2025-12-10 14:13:26 +0100

    builder

--- backend/src/main/java/com/flexisaf/admissions/entity/Student.java
+++ backend/src/main/java/com/flexisaf/admissions/entity/Student.java
@@ -2,6 +2,7 @@ package com.flexisaf.admissions.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "students")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Student {
